d3db887455482810a91fdea56c3ecbf78eba8e73
Bartosz Pazdur <b.pazdur@onet.pl>
2020-08-22 21:14:02 +0200

    Created crud implementation

--- routes/users.js
+++ routes/users.js
@@ -4,3 +4,6 @@ import UserController from '../app/controllers/UserController'
 const user = new UserController()
 
 app.get('/api/v1/users', async (req, res) => await user.index(req, res))
+app.get('/api/v1/users/:id', async (req, res) => await user.show(req, res))
+app.post('/api/v1/users/', async (req, res) => await user.store(req, res))
+app.put('/api/v1/users/:id', async (req, res) => await user.update(req, res))
