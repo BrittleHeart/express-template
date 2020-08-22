12b2544b05059868b3b101d6b9fc8d49b6f0ee0e
Bartosz Pazdur <b.pazdur@onet.pl>
2020-08-22 16:31:42 +0200

    Created users model

---
+++ app/User.js
@@ -0,0 +1,47 @@
+import Sequelize from 'sequelize'
+import connection from '../database/config'
+
+const Users = connection.define('users', {
+    userId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'Anonim'
+    },
+
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.NOW
+    },
+
+    deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+})
+
+export default Users
