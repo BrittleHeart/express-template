bbdb4d11ced4c3ca634260164f2aaaa57aab1af9
Bartosz Pazdur <b.pazdur@onet.pl>
2020-08-23 12:29:38 +0200

    Changed variable name

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ import AuthenticateController from '../app/controllers/AuthenticationController'
 import { verifyJWTToken } from '../app/middlewares/verifyJWTToken'
 
 const user = new UserController()
-const authenticate = new AuthenticateController()
+const auth = new AuthenticateController()
 
 app.get('/api/v1/users', verifyJWTToken, async (req, res) => await user.index(req, res))
 app.get('/api/v1/users/:id', verifyJWTToken, async (req, res) => await user.show(req, res))
@@ -12,4 +12,4 @@ app.post('/api/v1/users/', async (req, res) => await user.store(req, res))
 app.put('/api/v1/users/:id', verifyJWTToken, async (req, res) => await user.update(req, res))
 app.delete('/api/v1/users/:id', verifyJWTToken, async(req, res) => await user.destroy(req, res))
 
-app.post('/api/v1/authenticate', async (req, res) => await authenticate.authenticate(req, res))
+app.post('/api/v1/authenticate', async (req, res) => await auth.authenticate(req, res))
